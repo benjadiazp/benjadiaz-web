43af85aa28f581252c986152d0a4c92236b36c07
Benjamín Díaz <benjamindiaz@outlook.com>
2023-12-13 14:10:26 -0300

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import ClientProviders from "@/app/ClientProviders";
 import { unstable_setRequestLocale } from "next-intl/server";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 //const LatoFont = Lato({ weight: "400", subsets: ["latin"] });
 
@@ -33,7 +34,7 @@ export default function LocaleLayout({
     <html lang={locale} suppressHydrationWarning className={"scroll-smooth"}>
       <body
         className={`${fontSans.className} ${cn(
-          "bg-background min-h-screen font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
         )}  bg-neutral-50 text-black duration-150 dark:bg-neutral-900 dark:text-white`}
       >
@@ -42,6 +43,20 @@ export default function LocaleLayout({
           <Toaster />
         </ClientProviders>
       </body>
+      <Script id={"clarity"}>
+        {`<script type="text/javascript">
+          (function (c, l, a, r, i, t, y){
+          c[a] = c[a] || function () {
+            (c[a].q = c[a].q || []).push(arguments)
+          };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })
+        (window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}"
+        )
+        ;
+        </script>`}
+      </Script>
     </html>
   );
 }
