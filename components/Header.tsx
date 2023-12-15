e363f23f55ff3aa5227a9a7e71510abbc277f800
Benjamín Díaz <benjamindiaz@outlook.com>
2023-12-15 13:17:13 -0300

    Más mejoras visuales

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header
       className={
-        "sticky inset-0 z-50 mx-auto flex w-full flex-col flex-wrap items-center justify-center justify-center gap-4 px-4 py-4 backdrop-blur-lg sm:max-w-screen-2xl md:flex-row md:justify-between"
+        "sticky inset-0 z-50 mx-auto flex w-full flex-col flex-wrap items-center justify-center justify-center gap-4 border-b border-b-slate-50 bg-white bg-opacity-70 px-4 py-4 shadow-md backdrop-blur-lg dark:border-b-slate-700 dark:bg-slate-800 dark:bg-opacity-20 sm:max-w-screen-2xl md:flex-row md:justify-between"
       }
     >
       <Toggles />
